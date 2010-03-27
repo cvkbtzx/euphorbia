c3a5eaa42a3f0eef67cbb9b37b19c9de6ebfd1ef
Bzoloid <cvkbtzx@users.noreply.github.com>
2010-03-27 17:55:36 +0100

    Structure reorganization

--- share/euphorbia/icons-gen.sh
+++ share/euphorbia/icons-gen.sh
@@ -6,10 +6,12 @@ cd $(dirname "$0")
 for dim in 16 22 24 32 48 64 72 96 128 256 ; do
     inkscape --without-gui \
              --file=euphorbia.svg \
-             --export-png="euphorbia-${dim}.png" \
+             --export-png="../icons/hicolor/${dim}x${dim}/apps/euphorbia.png" \
              --export-width=${dim} \
              --export-height=${dim}
 done
+
+cp -v euphorbia.svg "../icons/hicolor/scalable/apps/euphorbia.svg"
 
 exit 0
 
