f21dc907b9b48442da80bf5460750a42d3e1a60e
Bzoloid <cvkbtzx@users.noreply.github.com>
2010-04-04 19:05:06 +0200

    Add licence text in source files

--- setup.py
+++ setup.py
@@ -5,21 +5,39 @@ import os.path
 from distutils.core import setup
 
 
-name = "EuphorbiaEditor"
-pkgs = [name] + ['.'.join([name,d]) for d in os.listdir(name) if os.path.isdir(os.path.join(name,d))]
+pkgname = "EuphorbiaEditor"
+pkgs = [pkgname] + ['.'.join([pkgname,d]) for d in os.listdir(pkgname) if os.path.isdir(os.path.join(pkgname,d))]
 datafiles = [(w[0],map(lambda f: os.path.join(w[0],f), w[2])) for w in os.walk("share") if len(w[2])>0]
+binfiles = ["euphorbia"]
 
+ver, lic = None, None
+f = open(os.path.join(pkgname,"__init__.py"))
+try:
+    for line in f:
+        l = line.split(" = ")
+        if l[0] == "__version__":
+            ver = l[1].strip("""'"\n\r""")
+        if l[0] == "__licence__":
+            lic = l[1].strip("""'"\n\r""")
+finally:
+    f.close()
+
+
+#------------------------------------------------------------------------------
 
 setup(
     name = "euphorbia",
-    version = '0.0.5',
+    version = ver,
     description = "Euphorbia LaTeX editor",
     author = "Bzoloid",
     author_email = "bzoloid@gmail.com",
     url = "http://code.google.com/p/euphorbia/",
-    license = 'GNU-GPL v2',
+    license = lic,
     packages = pkgs,
-    scripts = ["euphorbia"],
+    scripts = binfiles,
     data_files = datafiles
 )
+
+#------------------------------------------------------------------------------
+
 
