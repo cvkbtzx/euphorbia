7a72a631999c734811d0a1916794c6705099ceeb
Bzoloid <cvkbtzx@users.noreply.github.com>
2010-04-24 01:32:25 +0200

    New version

--- EuphorbiaEditor/__init__.py
+++ EuphorbiaEditor/__init__.py
@@ -21,8 +21,8 @@
 
 """Euphorbia LaTeX editor."""
 
-__version__ = '0.0.8'
-__date__    = '2010-04-14'
+__version__ = '0.0.9'
+__date__    = '2010-04-24'
 __authors__  = ['Bzoloid <bzoloid@gmail.com>']
 __license__ = 'GNU GPL v2'
 
